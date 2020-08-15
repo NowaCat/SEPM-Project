b0f20187b63ae781f408ff1ad7a9b398b8631973
Hung <s3715272@rmit.edu.vn>
2020-08-15 13:56:29 +1000

    initial commit

---
+++ tourManagementTool-backend/src/main/java/rmit/assignment/tourManagementTool/TourManagementToolApplication.java
@@ -0,0 +1,13 @@
+package rmit.assignment.tourManagementTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourManagementToolApplication.class, args);
+	}
+
+}
---
+++ tourManagementTool-backend/src/test/java/rmit/assignment/tourManagementTool/TourManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package rmit.assignment.tourManagementTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
